409dce2d36e9fdb085007eee1bc758962d51f824
Andrew Hamilton <andrew.hamilton@gmail.com>
2022-01-03 22:25:38 +0000

    handle multiple files and types inside ms tnef

--- parse_email.py
+++ parse_email.py
@@ -4,6 +4,7 @@ from PIL import Image
 from email.parser import BytesParser
 from tnefparse.tnef import TNEF, TNEFAttachment, TNEFObject
 import io
+import os
 
 def make_thumb(pil_image, orig_filename):
   pil_image.thumbnail((180,180))
@@ -23,21 +24,36 @@ def process_image(bytes, filename):
   pil_image = Image.open(io.BytesIO(bytes))
   make_thumb(pil_image, filename)
 
+def process_tnef(bytes):
+  tnef = TNEF(bytes, do_checksum=True)
+  for a in tnef.attachments:
+    content_type = None
+    for attr in a.mapi_attrs:
+      if attr.attr_type == 31 and attr.name == 14094:   # stupid microsoft enums
+        content_type = attr.raw_data[0].rstrip('\x00')  # stupid microsoft nulls
+    process_attachment(a.name, a.data, content_type)
+
+def process_attachment(filename, payload, content_type):
+  if not content_type:
+    ext = os.path.splitext(filename)[1]
+    content_type = {
+      '.pdf': 'application/pdf'
+    }.get(ext)
+  if content_type:
+    if content_type == 'application/ms-tnef':
+      process_tnef(payload)
+    elif content_type.startswith('image/'):
+      process_image(payload, filename)
+    elif content_type == 'application/pdf':
+      process_pdf(payload, filename)
+
 f = open(sys.argv[1], 'rb')
 msg = BytesParser().parse(f)
 
 for part in msg.walk():
   filename = part.get_filename()
-  [maintype, subtype] = part.get_content_type().split('/', 1)
+  content_type = part.get_content_type()
   payload = part.get_payload(None, decode=True)
-  if subtype == 'ms-tnef':
-    tnef = TNEF(payload, do_checksum=True)
-    for a in tnef.attachments:
-      with open(a.name, "wb") as afp:
-        afp.write(a.data)
-  elif maintype == 'image':
-    process_image(payload, filename)
-  elif subtype == 'pdf':
-    process_pdf(payload, filename)
+  process_attachment(filename, payload, content_type)
 
 f.close()
